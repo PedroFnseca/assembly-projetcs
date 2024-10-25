676124692bc19eb6c698621109c708ded687f530
Pedro Fonseca <pedropereira.pp264@gmail.com>
2024-10-25 20:00:33 -0300

    ref: add comments char input

--- charinput.asm
+++ charinput.asm
@@ -2,16 +2,20 @@
 .data
   msg db "Digite um caractere: $"
 .code
+  ; Carrega a memoria ram no registrador
   mov ax, @data
   mov ds, ax
 
+  ; Execita o print do valor da memoria ram 
   lea dx, msg
-  mov aa, 09h
+  mov ah, 09h
   int 21h
 
+  ; Coleta o char do usuario
   mov ah, 01h
   int 21h
 
+  ; Pega o valor que veio do input e print
   mov dl, al
   mov ah, 02h
   int 21h
